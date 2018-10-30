e54ad4371c8418d68d07dd30e4d69d4b4e71b865
edlikys <43602811+edlikys@users.noreply.github.com>
2018-10-30 09:07:25 +0300

    with function

---
+++ lab1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def number(): 
+             x=int(input()) 
+             if x <= 0: 
+                     print (x) 
+             else: 
+                 print (-x)
+
